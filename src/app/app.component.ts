5eae4f8adc09f629bd0e2364a42411811feac0a6
9Luigi <fastromankudrik@gmail.com>
2023-06-09 21:48:36 +0600

    implementsGlobalStyles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,10 @@ import { User } from './user';
 
 @Component({
     selector: 'app-component',
-    templateUrl: 'app.component.html'
+    templateUrl: 'app.component.html',
 })
 export class AppComponent {
-    @ViewChild('readOnlyTemplate', { static: false }) readOnlyTemplate: TemplateRef<any> | undefined;
-    @ViewChild('editTemplate', { static: false }) editTemplate: TemplateRef<any> | undefined;
 
-    editedUser: User | null = null;
-    users: User[];
-    isNewRecord: boolean = false;
-    statusMessage: string = "";
 
 
     constructor() { }
